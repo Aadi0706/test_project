03ac5ed63c153010e28447de91880a2e1655eea2
Aditya Choubey <97503774+Aadi0706@users.noreply.github.com>
2022-04-18 19:30:35 +0530

    Add files via upload

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,147 @@
+import { useState } from "react";
+import "./AddStudent.css";
+export const AddStudent = () => {
+  const [error, seterror] = useState(false)
+  const [formData,setform]=useState({
+    first_name:"",
+    last_name:"",
+    email:"",
+    gender:0,
+    age: 0,
+    tenth_score: 0,
+    twelth_score:0 ,
+    preferred_branch: 0,
+  });
+  const handlechange=(e)=>{
+    const {name,value} = e.target;
+    setform({
+        ...formData,
+        [name]:value,
+    })
+}
+  async function postdata(formData){
+    if(formData.age>=100 || formData.first_name==="" || formData.twelth_score >=100 || formData.tenth_score>=100 ){
+      seterror(true);
+      return ;
+    }else{
+      var data2 = await fetch("http://localhost:8080/students",{
+      method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+    })
+    }
+    
+  }
+
+
+  return (
+    <form className="addstudent">
+      <div>
+        Firstname:{" "}
+        <input
+          type="text"
+          name="first_name"
+          className="first_name"
+          placeholder="enter first name"
+          onChange={handlechange}
+        />
+      </div>
+      <div>
+        {" "}
+        Last Name:
+        <input
+          type="text"
+          name="last_name"
+          className="last_name"
+          placeholder="enter last name"
+          onChange={handlechange}
+        />
+      </div>
+      <div>
+        {" "}
+        Email:
+        <input
+          type="email"
+          name="email"
+          className="email"
+          placeholder="enter email"
+          onChange={handlechange}
+        />
+      </div>
+
+      <div>
+        Gender: {/* NOTE: radio boxes only work when they have same `name`. */}
+        <div>
+          Male
+          <input
+            onChange={handlechange}
+            name="gender"
+            className="male"
+            type="radio"
+            value={"male"}
+          />{" "}
+          Female{" "}
+          <input
+            name="gender"
+            className="female"
+            type="radio"
+            value={"female"}
+            onChange={handlechange}
+          />
+        </div>
+      </div>
+      <div>
+        Age{" "}
+        <input
+          type="number"
+          name="age"
+          className="age"
+          placeholder="enter age"
+          onChange={handlechange}
+        />
+      </div>
+      <div>
+        Tenth Score:{" "}
+        <input
+          type="number"
+          name="tenth_score"
+          className="tenth_score"
+          placeholder="enter 10th score"
+          onChange={handlechange}
+        />{" "}
+      </div>
+      <div>
+        Twelth Score:{" "}
+        <input
+          type="number"
+          name="twelth_score"
+          className="twelth_score"
+          placeholder="enter 12th score"
+          onChange={handlechange}
+        />{" "}
+      </div>
+      <div>
+        <select
+          value={""} // select dropdown needs both value and onChange attributes
+          name="preferred_branch"
+          className="preferred_branch"
+          onChange={handlechange}
+        >
+          <option value="law" onChange={handlechange}>law</option>
+          <option value="commerce" onChange={handlechange}>commerce</option>
+          <option value="science" onChange={handlechange}>science</option>
+          <option value="sports" onChange={handlechange}>sports</option>
+          <option value="arts" onChange={handlechange}>arts</option>
+          <option value="acting" onChange={handlechange}>acting</option>
+        </select>
+      </div>
+
+      <input className="submit" type="submit" value="Submit" onClick={()=>{postdata(formData)}}/>
+      {
+        <div className="error"> {error ? "fill the form properly" : null}</div>
+        // show this div with proper error before submitting form, if there's anything not provided
+        // eg: first name missing, age cannot be greater than 100 etc
+      }
+    </form>
+  );
+};
---
+++ src/components/ShowStudents.jsx
@@ -0,0 +1,185 @@
+import { useEffect, useState } from "react";
+
+export const ShowStudents = () => {
+  const [studentData, setStudentData] = useState([]);
+  
+  useEffect(()=>{
+    async function getdata(){
+      var data = await fetch("http://localhost:8080/students")
+      var res = await data.json();
+      setStudentData(res)
+    }
+    getdata()
+  },[])
+  return (
+    <div>
+      <div className="controls">
+        <div>
+          Sort By:{" "}
+          <select
+            // select dropdown needs both value and onChange
+            className="sortby"
+          >
+            <option value="first_name">First Name</option>
+            <option value="gender">Gender</option>
+            <option value="age">Age</option>
+            <option value="tenth_score">10th Score</option>
+            <option value="twelth_score">12th Score</option>
+          </select>
+        </div>
+        <div>
+          Order:
+          <select
+            // select dropdown needs both value and onChange
+            className="sortorder"
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+        <button className="sort">sort</button>
+      </div>
+      <table className="table" border="1">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>10th Score</th>
+            <th>12th Score</th>
+            <th>Branch</th>
+          </tr>
+        </thead>
+        <tbody className="tbody">
+          {/* populate all rows like below: */}
+          {studentData.map((e)=>{
+            return <tr className="row">
+            <td className="first_name">{e.first_name}</td>
+            <td className="last_name">{e.last_name}</td>
+            <td className="email">{e.email}</td>
+            <td className="gender">{e.gender}</td>
+            <td className="age">{e.age}</td>
+            <td className="tenth_score">{e.tenth_score}</td>
+            <td className="twelth_score">{e.twelth_score}</td>
+            <td className="preferred_branch">{e.preferred_branch}</td>
+          </tr>
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+ 
+                 // Show Students with sorting done.//
+
+// import { useEffect, useState } from "react";
+
+// export const ShowStudents = () => {
+//   const [studentData, setStudentData] = useState([]);
+//   const [sort,setsort] = useState({
+//     sortby:"",
+//     sortorder:"",
+//   })
+//   useEffect(()=>{
+//     async function getdata(){
+//       var data = await fetch("http://localhost:8080/students")
+//       var res = await data.json();
+//       setStudentData(res)
+//     }
+//     getdata()
+//   },[])
+   
+//   function sortData(e){
+//     const {className,value} = e.target;
+//     setsort({
+//         ...sort,
+//         [className]:value,
+//     })
+//   }
+//   function sohorting(data){
+//     var x =data.sortby;
+//     if(data.sortorder==="asc" && x== "age"){
+//       setStudentData(studentData.sort((a,b)=>{return Number(a.age)-Number(b.age)}))
+//     }else if(data.sortorder==="desc" && x=="age"){
+//      setStudentData(studentData.sort((a,b)=>{return Number(b.age)-Number(a.age)}))
+//     }
+//     if(data.sortorder==="asc" && x== "tenth_score"){
+//       setStudentData(studentData.sort((a,b)=>{return Number(a.tenth_score)-Number(b.tenth_score)}))
+//     }else if(data.sortorder==="desc" && x=="tenth_score"){
+//      setStudentData(studentData.sort((a,b)=>{return Number(b.tenth_score)-Number(a.tenth_score)}))
+//     }
+//     if(data.sortorder==="asc" && x== "twelth_score"){
+//       setStudentData(studentData.sort((a,b)=>{return Number(a.twelth_score)-Number(b.twelth_score)}))
+//     }else if(data.sortorder==="desc" && x=="twelth_score"){
+//      setStudentData(studentData.sort((a,b)=>{return Number(b.twelth_score)-Number(a.twelth_score)}))
+//     }
+//   }
+//   useEffect(()=>{
+//     sohorting(sort)
+
+//   },[studentData])
+
+//   return (
+//     <div>
+//       <div className="controls">
+//         <div>
+//           Sort By:{" "}
+//           <select
+//             // select dropdown needs both value and onChange
+//             className="sortby"
+//             onChange={sortData}
+//           >
+//             <option value="first_name">First Name</option>
+//             <option value="gender">Gender</option>
+//             <option value="age">Age</option>
+//             <option value="tenth_score">10th Score</option>
+//             <option value="twelth_score">12th Score</option>
+//           </select>
+//         </div>
+//         <div>
+//           Order:
+//           <select
+//             // select dropdown needs both value and onChange
+//             className="sortorder"
+//             onChange={sortData}
+//           >
+//             <option value="asc">Ascending</option>
+//             <option value="desc">Descending</option>
+//           </select>
+//         </div>
+//         <button className="sort" onClick={()=>{sohorting(sort)}}>sort</button>
+//       </div>
+//       <table className="table">
+//         <thead>
+//           <tr>
+//             <th>First Name</th>
+//             <th>Last Name</th>
+//             <th>Email</th>
+//             <th>Gender</th>
+//             <th>Age</th>
+//             <th>10th Score</th>
+//             <th>12th Score</th>
+//             <th>Branch</th>
+//           </tr>
+//         </thead>
+//         <tbody className="tbody">
+//           {studentData.map((e)=>{
+//             return <tr className="row">
+//             <td className="first_name">{e.first_name}</td>
+//             <td className="last_name">{e.last_name}</td>
+//             <td className="email">{e.email}</td>
+//             <td className="gender">{e.gender}</td>
+//             <td className="age">{e.age}</td>
+//             <td className="tenth_score">{e.tenth_score}</td>
+//             <td className="twelth_score">{e.twelth_score}</td>
+//             <td className="preferred_branch">{e.preferred_branch}</td>
+//           </tr>
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
